0a81fde69a0fdbec4899d5d6a141bd856f829bfe
Gabriel Lau <okatsura.lau@gmail.com>
2014-12-17 13:47:02 -0300

    remoção de anotações na entidade fictícia Usuario para evitar que ela seja adicionada ao banco de dados

--- src/lojadsc/entidades/Usuario.java
+++ src/lojadsc/entidades/Usuario.java
@@ -2,15 +2,13 @@ package lojadsc.entidades;
 
 import java.io.Serializable;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
+/**
+ * Entidade fictícia (não deve salvar no banco)
+ *
+ * @author Gabriel
+ */
 public class Usuario implements Serializable {
 	private static final long serialVersionUID = -6467792829340036088L;
-	private int id;
 	private String nome, login, password;
 
 	public Usuario() {
@@ -19,20 +17,9 @@ public class Usuario implements Serializable {
 
 	public Usuario(String nome, String login, String password) {
 		super();
-		this.id = 42;
 		this.nome = nome;
 		this.login = login;
 		this.password = password;
-	}
-
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
 	}
 
 	public String getNome() {
